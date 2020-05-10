d42b39e9df343d62f064ddbef511031db9487d41
Eninnv <63240742+Eninnv@users.noreply.github.com>
2020-05-10 20:01:12 +0300

    Update cli.php

--- eninnv/cli.php
+++ eninnv/cli.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $n = $i = 5;
 
@@ -7,14 +6,4 @@ while ($i--) {
 }
 echo str_repeat(' ', 2).str_repeat('* ', 3)."\n";
 echo str_repeat(' ', 2).str_repeat('* ', 3)."\n";
-=======
-<?php
-$n = $i = 5;
-
-while ($i--) {
-    echo str_repeat(' ', $i).str_repeat('* ', $n - $i)."\n";
-}
-echo str_repeat(' ', 2).str_repeat('* ', 3)."\n";
-echo str_repeat(' ', 2).str_repeat('* ', 3)."\n";
->>>>>>> b270548f4f7cc53a80f86ee3b4c127a00e3f2e0c
 echo "Hello World!"; echo "\n";
