cacf36bb1004bb43c5cc041fd0fa273e0b24fe68
Eninnv <63240742+Eninnv@users.noreply.github.com>
2020-05-10 20:29:23 +0300

    selenium

---
+++ eninnv/check_sum/lib.py
@@ -0,0 +1,4 @@
+def input_clear_and_type(local_driver, locator, value):
+    local_input = local_driver.find_element_by_id(locator)
+    local_input.clear()
+    local_input.send_keys(value) 
---
+++ eninnv/check_sum/locators.py
@@ -0,0 +1,8 @@
+sum_page = {
+    "close_btn": "//*[@id='at-cv-lightbox-close']",
+    "input_a_id": "sum1",
+    "input_b_id": "sum2",
+    "get_total_button_xpath": "//button[@onclick='return total()']",
+    "actual_output_id": "displayvalue"
+
+}
