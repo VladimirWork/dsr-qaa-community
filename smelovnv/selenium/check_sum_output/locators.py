1bb6fca05c4e86789b0cdf4d6c96188485f1f5a7
NikolaiSmelov <yarosmilk@gmail.com>
2020-05-13 00:04:17 +0300

    Add my locators and fix for selenium_test script

---
+++ smelovnv/selenium/check_sum_output/locators.py
@@ -0,0 +1,9 @@
+# Provide correct locators in order to make the script work:
+sum_page = {
+    "input_a_id": "sum1",
+    "input_b_id": "sum2",
+    "get_total_button_xpath": "//button[@onclick='return total()']",
+    "actual_output_id": "displayvalue",
+    "close_button": "//*[@id='at-cv-lightbox-close']"
+
+}
