d4e0b8d9cc9a8f7a4a7c4373c850d8bab597fbd2
Vladimir S <vladimir.shishkin@dsr-corporation.com>
2020-05-28 18:57:36 +0300

    Delete locators.py

--- selenium/check_sum_output/locators.py
+++
@@ -1,8 +0,0 @@
-# Provide correct locators in order to make the script work:
-sum_page = {
-    "input_a_id": "sum1",
-    "input_b_id": "sum2",
-    "get_total_button_xpath": "/html/body/div[2]/div/div[2]/div[2]/div[2]/form/button",
-    "actual_output_id": "displayvalue",
-    "close_button_xpath": "/html/body/div[3]/div/div[1]/div/div[2]/a"
-}
